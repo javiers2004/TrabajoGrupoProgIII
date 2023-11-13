7fb2ec6721d5e112d4cc91dbc179de842633916c
alejandrojjrr <alejandrovm4002@gmail.com>
2023-11-13 23:34:46 +0100

    Implementacion JTree al boton Estadistica con guardado en fichero.

--- TrabajoGrupoProgIII/src/VentanaInicio.java
+++ TrabajoGrupoProgIII/src/VentanaInicio.java
@@ -1,7 +1,22 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -12,6 +27,12 @@ public class VentanaInicio extends JFrame implements ActionListener {
     private JButton botonPlay, boton4, botonEstads, botonNombre;
     private JLabel lblTitulo;
     private JFrame va;
+    
+    private String nombreUsuario; // Variable para almacenar el nombre
+    private DefaultMutableTreeNode rootNode; // Nodo raíz para el árbol
+    private static final String NOMBRES_FILE = "nombres.txt";
+    private ArrayList<String> nombres; // Lista para almacenar los nombres
+
     
     private VentanaAudio player;
     private VentanaAjustes ventanaAjustes;
@@ -37,6 +58,10 @@ public class VentanaInicio extends JFrame implements ActionListener {
 		pEste = new JPanel(new GridLayout(2,1));
 		pOeste = new JPanel();
 	
+
+
+		
+		
 		
 		/*AÑADIR LOS PANELES AL PANEL PRINCIPAL DE LA VENTANA*/
 		getContentPane().add(pNorte, BorderLayout.NORTH);
@@ -44,6 +69,9 @@ public class VentanaInicio extends JFrame implements ActionListener {
 		getContentPane().add(pEste, BorderLayout.EAST);
 		getContentPane().add(pOeste, BorderLayout.WEST);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
+			
+		
+		
 		
 		/*CREACIÓN DE COMPONENTES*/	
 		lblTitulo = new JLabel("BIENVENIDO A +EL JUEGO+ ");
@@ -105,18 +133,63 @@ public class VentanaInicio extends JFrame implements ActionListener {
         botonNombre.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
     
-    /*EVENTOS*/
-    botonNombre.addActionListener(new ActionListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String nombre = JOptionPane.showInputDialog("Introduce tu nombre:");
-            if (nombre != null) {
-                JOptionPane.showMessageDialog(null, "Hola, " + nombre + "!");
-            }
-		}
-	});
+        rootNode = new DefaultMutableTreeNode("Estadísticas"); // Inicializar rootNode
+        nombres = new ArrayList<>();
+        cargarArbolNombre();
 
+    /*EVENTOS*/
+        botonNombre.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Verificar si el nombre ya se ha configurado
+                if (nombreUsuario == null) {
+                    // Si el nombre no se ha configurado, solicitarlo al usuario
+                    nombreUsuario = JOptionPane.showInputDialog("Introduce tu nombre:");
+                    if (nombreUsuario != null) {
+                        JOptionPane.showMessageDialog(null, "Hola, " + nombreUsuario + "!");
+                        botonNombre.setEnabled(false); // Deshabilitar el botón después de ingresar el nombre
+                        nombres.add(nombreUsuario); // Agregar el nombre a la lista
+                       // guardarArbolNombre();
+
+
+                    }
+                }
+            }
+        });
+    
+    
+    
+    botonEstads.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Verificar si el nombre ya se ha configurado
+            if (nombreUsuario == null) {
+                // Si el nombre no se ha configurado, solicitarlo al usuario
+                nombreUsuario = JOptionPane.showInputDialog("Introduce tu nombre:");
+                if (nombreUsuario != null) {
+                    JOptionPane.showMessageDialog(null, "Hola, " + nombreUsuario + "!");
+                    nombres.add(nombreUsuario); // Agregar el nombre a la lista
+
+                }
+            }
+
+            // Crear un JTree con la carpeta inicial "estadisticas"
+            rootNode = new DefaultMutableTreeNode("Estadísticas");
+            
+            cargarArbolNombre();
+            
+            // Si el nombre se ha configurado, agregarlo como un nodo
+            if (nombreUsuario != null) {
+                rootNode.add(new DefaultMutableTreeNode(nombreUsuario));
+            }
+
+            // Mostrar el árbol en un cuadro de diálogo
+            JOptionPane.showMessageDialog(null, new JScrollPane(new JTree(rootNode)));
+
+            // Guardar el árbol con el nombre en el archivo
+            guardarArbolNombre();
+        }
+    });
  
  botonPlay.addActionListener(new ActionListener() {
 
@@ -149,6 +222,9 @@ public class VentanaInicio extends JFrame implements ActionListener {
 			hilorobot.start();
 		}
 	});
+ 
+ 	
+ 	
       
     
     boton4.addActionListener(new ActionListener() {
@@ -168,6 +244,54 @@ public class VentanaInicio extends JFrame implements ActionListener {
     
     
     }
+    // Método para cargar el árbol con los nombres desde el archivo
+
+ // Método para guardar los nombres en el archivo (solo una vez)
+ // Método para cargar los nombres desde el archivo y agregarlos al árbol
+ // Método para cargar los nombres desde el archivo y agregarlos al árbol
+ // Método para cargar los nombres desde el archivo y agregarlos al árbol
+    private void cargarArbolNombre() {
+        File archivo = new File(NOMBRES_FILE);
+
+        // Verificar si el archivo no existe y crearlo si es necesario
+        if (!archivo.exists()) {
+            try {
+                archivo.createNewFile(); // Crea el archivo si no existe
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (archivo.exists() && archivo.isFile() && archivo.length() > 0) {
+            try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    // Agregar cada nombre como un nodo al árbol
+                    DefaultMutableTreeNode nombreNode = new DefaultMutableTreeNode(linea);
+                    rootNode.add(nombreNode);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+    // Método para guardar la lista completa de nombres en el archivo al cierre de la aplicación
+    private void guardarArbolNombre() {
+        File archivo = new File(NOMBRES_FILE);
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(archivo, true))) {
+            for (String nombre : nombres) {
+                pw.println(nombre);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     
     
 	@Override
