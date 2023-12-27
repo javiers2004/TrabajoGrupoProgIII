7a305722165642f42c2299a8d6e625f363f2bcf9
elenaatorralbo <elena.torralbo@opendeusto.es>
2023-12-27 19:23:26 +0100

    .

--- TrabajoGrupoProgIII/src/Jugador.java
+++ TrabajoGrupoProgIII/src/Jugador.java
@@ -211,13 +211,12 @@ public class Jugador {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		this.posx = (int)(57);
 		this.posy = (int)(431);		
-<<<<<<< HEAD
 		/*this.posx = (int)(1000);
 		this.posy = (int)(10000);*/
-=======
-//		this.posx = (int)(1000);
-//		this.posy = (int)(10000);
->>>>>>> refs/remotes/origin/master
+
+this.posx = (int)(1000);
+		this.posy = (int)(10000);
+
 		
 		this.vidatotal = 100;
 		this.vidarestante = 100;
