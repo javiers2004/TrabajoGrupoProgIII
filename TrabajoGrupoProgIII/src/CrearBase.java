a1d119fce2b2787a5b8f72faaa97e7b5e1f3c621
ibonarambarri <ibon.arambarri@opendeusto.es>
2023-12-27 17:27:19 +0100

    Signed-off-by: ibonarambarri <ibon.arambarri@opendeusto.es>

--- TrabajoGrupoProgIII/src/CrearBase.java
+++ TrabajoGrupoProgIII/src/CrearBase.java
@@ -15,7 +15,30 @@ public static void main(String[] args) {
 			Connection connection = DriverManager.getConnection("jdbc:sqlite:basededatosdelaspartidas.db");			//ESTA URL PUEDE SER TANTO LOCAL COMO REMOTA
 			java.sql.Statement statement = connection.createStatement();
 			statement.executeUpdate("DROP TABLE IF EXISTS PARTIDAS");
-			statement.executeUpdate("CREATE TABLE PARTIDAS(NOMBRE VARCHAR, NIVEL INT, EXPERIENCIA INT,VIDA INT, POSX INT, POSY INT, VIDATOTAL INT,NUMERODEGOLPES INT, DISTANCE INT, GOLPESEFECTIVOS INT, DANOINFLINGIDO INT, DANORECIBIDO INT, STAMINATOTAL INT, SLIMES INT, PAJAROS INT, MURCIELAGOS INT, CAPARAZONES INT, PUERCOESPINES INT, GOBLINS INT, MVIDA INT, MVELOCIDAD INT, MATAQUE INT, CONSUMIBLES INT, PRIMARY KEY(NOMBRE))");
+			statement.executeUpdate("CREATE TABLE PARTIDAS(NOMBRE VARCHAR, "
+					+ "NIVEL INT, "
+					+ "EXPERIENCIA INT,"
+					+ "VIDA INT, "
+					+ "POSX INT, "
+					+ "POSY INT, "
+					+ "VIDATOTAL INT,"
+					+ "NUMERODEGOLPES INT, "
+					+ "DISTANCE INT, "
+					+ "GOLPESEFECTIVOS INT, "
+					+ "DANOINFLINGIDO INT, "
+					+ "DANORECIBIDO INT, "
+					+ "STAMINATOTAL INT, "
+					+ "SLIMES INT, "
+					+ "PAJAROS INT, "
+					+ "MURCIELAGOS INT, "
+					+ "CAPARAZONES INT, "
+					+ "PUERCOESPINES INT, "
+					+ "GOBLINS INT, "
+					+ "MVIDA INT, "
+					+ "MVELOCIDAD INT, "
+					+ "MATAQUE INT, "
+					+ "CONSUMIBLES INT, "
+					+ "PRIMARY KEY(NOMBRE))");
 			statement.executeUpdate("DROP TABLE IF EXISTS OBJETOS");
 			//statement.executeUpdate("CREATE TABLE OBJETOS(CODIGO INT, NOMBRE VARCHAR, PRIMARY KEY (CODIGO)");
 		}catch (SQLException e) {
